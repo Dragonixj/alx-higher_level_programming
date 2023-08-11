c270fa99d64cd43190f610e70923a2c718026e91
Dragonixj <delafiado@gmail.com>
2023-08-11 09:13:58 +0000

    made some changes

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+    """Prints the addition, multiple, quotient and the division of a and b"""
     from calculator_1 import add, sub, mul, div
 
 a = 10
