051fba6144f3f9eb72939b7a10882f82700f0a4c
Dragonixj <delafiado@gmail.com>
2023-08-14 20:23:33 +0000

    Task 8 modified

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) < 2:
         if len(tuple_a) == 0:
@@ -14,5 +12,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
         else:
             tuple_b = tuple_b[0], 0
 
-    add_tuple = tuple_a[0] + tuple_b[0], tuple_a[0] + tuple_b[1]
+    add_tuple = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
     return add_tuple
