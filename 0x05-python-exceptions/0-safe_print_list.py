bb87ce38987aed9bb47712c51a614f3f60663c86
Dragonixj <delafiado@gmail.com>
2023-08-28 21:54:32 +0000

    Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def safe_print_list(my_list=[], x=0):
     number = 0
     for i in range(x):
@@ -8,5 +9,5 @@ def safe_print_list(my_list=[], x=0):
             number += 1
         except IndexError:
             break
-        print()
-        return number
+    print()
+    return number
