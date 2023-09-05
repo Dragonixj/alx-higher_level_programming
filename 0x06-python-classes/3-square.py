bdca996df34f665fd0672969d18a8189ef1a1a69
Dragonixj <delafiado@gmail.com>
2023-09-05 19:02:24 +0000

    task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 class Square:
     """Square with a private object attribute size"""
 
@@ -21,4 +22,4 @@ class Square:
         Returns: the area of the square
         """
 
-        return self.__size ** 2
+        return self.__size**2
