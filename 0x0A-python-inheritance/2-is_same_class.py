2f9c71ebadcf0103272201fbdff8de42d619ed45
Dragonixj <delafiado@gmail.com>
2023-09-11 15:13:44 +0000

    Task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module contains the is_same_class"""
+
+
+def is_same_class(obj, a_class):
+    """Returns True if the object is exactly an isntance of the specified class"""
+    return type(obj) == a_class
