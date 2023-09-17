824fef45d5894986a9996666c75aa00037be9ce9
Dragonixj <delafiado@gmail.com>
2023-09-17 19:17:12 +0000

    Task 2 initial commit

---
+++ 0x0C-python-almost_a_circle/rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""module that contains class Rectanglele inheriting from Base(models.base)"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Defines a Rectangle class"""
+
+    pass
