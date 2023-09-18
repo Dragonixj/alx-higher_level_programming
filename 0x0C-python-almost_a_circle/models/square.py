0dc46d48061df25ca3b601b650c81d2d3e4151a2
Dragonixj <delafiado@gmail.com>
2023-09-18 20:32:37 +0000

    Task 10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module that defines a square object"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Defines a square class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Method that initialises the square
+        Args:
+            size: size of the sides of a square
+            x: position on the x axis
+            y: position on the y axis
+        Return:
+            Always nothing"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Method that returns a string"""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
