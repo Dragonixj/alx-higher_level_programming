3dce76fb4cd111cf8d4117f1aa002e506cd58d17
Dragonixj <delafiado@gmail.com>
2023-11-18 13:55:17 +0000

    some syntax formatting

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 """Lists all states from the database hbtn_0e_0_usa """
-import MySQLdb
 import sys
 
+import MySQLdb
 
 if __name__ == "__main__":
-    db = MYSQLdb.connect(host="localhost",user=sys.argv[1],
-                        passwd=sys.argv[2], db=sys.argv[2], port=3306)
+    db = MYSQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[2],
+        port=3306,
+    )
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
