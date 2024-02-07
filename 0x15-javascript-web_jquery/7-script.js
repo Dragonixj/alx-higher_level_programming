3076c563f5cb2f640c0153323de6be5fd97811ee
Dragonixj <delafiado@gmail.com>
2024-02-07 23:07:31 +0000

    Task all

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,4 +1,6 @@
-$.get('https://swapi.co/api/people/5/?format=json', function (data) {
-    $('DIV#character').text(data.name);
-});
-
+$.get(
+    'https://swapi-api.alx-tools.com/api/people/5/?format=json',
+    function (data) {
+        $('DIV#character').text(data.name);
+    },
+);
--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,5 +1,4 @@
-$.get('https://swapi.co/api/films/?format=json', function (data) {
-    $('UL#list_movies').append(
-        ...data.results.map((movie) => `<li>${movie.title}</li>`),
-    );
+$.get(
+    'https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+    $('UL#list_movies').append(...data.results.map((movie => `<li>${movie.title}</li>`));
 });
